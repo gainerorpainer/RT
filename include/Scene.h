02309542e164830a92ec2fdf85e7daf3750542a7
Lorenz Georg Görne <lorenzgeorg.goerne@bosch.com>
2024-01-31 17:25:30 +0100

    egal

--- include/Scene.h
+++ include/Scene.h
@@ -1,15 +1,16 @@
 #pragma once
 
-#include <array>
+#include <vector>
 
-#include <Shapes.h>
+#include "Shapes.h"
 
 namespace Scene
 {
     using namespace Shapes;
 
-    std::array<Shape *, 4> Objects = {
-        new Plane{"Sunplane", MaterialInfo::MakeEmitting({255, 255, 255}), {-1000, -1000, 1000}, {1, 1, -1}},
+    std::vector<Shape *> const Objects = {
+        new Plane{"Sunplane", MaterialInfo::MakeEmitting({255, 255, 255}), {-100, 0, 100}, {0.5, 0, -1}},
+        new Sphere{"Skysphere", MaterialInfo::MakeEmitting({50, 100, 255}), {0, 0, 0}, 1000},
         new Plane{"Floor", MaterialInfo::MakeAbsorbing({100, 100, 100}), {0, 0, 0}, {0, 0, 1}},
         new Sphere{"Red Sphere", MaterialInfo::MakeAbsorbing({230, 25, 25}), {3, -1, 1}, 1},
         new Sphere{"Blue Sphere", MaterialInfo::MakeAbsorbing({25, 25, 230}), {4, 1, 1.5}, 1.5}};
