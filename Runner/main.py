979584b431a82ccc51d598e562808cbbf2d8b511
PainerOrGainer <istmirechtegal@outlook.com>
2024-02-10 18:26:34 +0100

    Before Opt

--- Runner/main.py
+++ Runner/main.py
@@ -1,3 +1,4 @@
+"""Runtime to watch rt.dll and run it on change"""
 import os
 import time
 import ctypes
@@ -12,39 +13,52 @@ DLLPATH = "rt.dll"
 WINDOWNAME = "Output"
 DOWNSIZE_FACTOR = 2
 
-last_modified = 0
 
-cv2.namedWindow(WINDOWNAME)
-cv2.setWindowProperty(WINDOWNAME, cv2.WND_PROP_TOPMOST, 1)
+def main():
+    """Entry point
+    """
+    last_modified = 0
 
-while True:
-    modified = os.stat(DLLPATH).st_mtime if os.path.exists(DLLPATH) else 0
-    if modified != last_modified:
-        print("Modified, re-running...")
-        
-        try:
-            # make a copy to give not lock access to dll
-            temp_dll = os.path.join(tempfile.gettempdir(), f"{uuid.uuid4()}.dll")
-            shutil.copy2(DLLPATH, temp_dll)
+    cv2.namedWindow(WINDOWNAME)
+    cv2.setWindowProperty(WINDOWNAME, cv2.WND_PROP_TOPMOST, 1)
 
-            dll = ctypes.WinDLL(temp_dll, winmode=0)
-        except (PermissionError, OSError):
-            print("File locked/invalid...")
-            time.sleep(0.25)
-            continue
+    while True:
+        modified = os.stat(DLLPATH).st_mtime if os.path.exists(DLLPATH) else 0
+        if modified != last_modified:
+            print("Modified, re-running...")
 
-        last_modified = modified
+            try:
+                # make a copy to give not lock access to dll
+                temp_dll = os.path.join(
+                    tempfile.gettempdir(), f"{uuid.uuid4()}.dll")
+                shutil.copy2(DLLPATH, temp_dll)
 
-        now = time.time()
-        dll.main()
-        print(f"Runtime: {1000 * (time.time() - now):0.1f}ms")
+                dll = ctypes.WinDLL(temp_dll, winmode=0)
+            except (PermissionError, OSError):
+                print("File locked/invalid...")
+                time.sleep(0.25)
+                continue
 
-        # most clean way to release dll
-        del dll
-        
-        image = cv2.imread(IMGPATH)
-        image = cv2.resize(image, (image.shape[1] // DOWNSIZE_FACTOR, image.shape[0] // DOWNSIZE_FACTOR), interpolation=cv2.INTER_LINEAR)
-        cv2.imshow(WINDOWNAME, image)
+            last_modified = modified
 
-    time.sleep(0.25)
-    cv2.pollKey()
+            now = time.time()
+            dll.main()
+            print(f"Runtime: {1000 * (time.time() - now):0.1f}ms")
+
+            # most clean way to release dll
+            del dll
+
+            image = cv2.imread(IMGPATH)
+            image = cv2.resize(
+                image, (image.shape[1] // DOWNSIZE_FACTOR, image.shape[0] // DOWNSIZE_FACTOR), interpolation=cv2.INTER_LINEAR)
+            cv2.imshow(WINDOWNAME, image)
+
+        time.sleep(0.25)
+        key = cv2.pollKey()
+        if key == 32:
+            print("Reset by user")
+            last_modified = 0
+
+
+if __name__ == "__main__":
+    main()
